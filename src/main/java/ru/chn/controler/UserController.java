ff6c887f8f359803e77ae54a46fd084c13bd9ac1
Vladislav <watislove4u@gmail.com>
2024-05-13 17:18:20 +0400

    add user get all get current by token

--- src/main/java/ru/chn/controler/UserController.java
+++ src/main/java/ru/chn/controler/UserController.java
@@ -3,6 +3,7 @@ package ru.chn.controler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.chn.dto.response.MessageResponse;
 import ru.chn.dto.response.UserDetailResponse;
 import ru.chn.dto.response.UsersPreviewsResponse;
+import ru.chn.security.jwt.JwtUtils;
 import ru.chn.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,6 +23,10 @@ public class UserController {
 
     private final UserService userService;
 
+    @Autowired
+    JwtUtils jwtUtils;
+
+    // get user details by id
     @GetMapping("/{id}")
     public ResponseEntity<?> getUserById(@PathVariable Long id) {
         UserDetailResponse resp = userService.getUserDetailById(id);
@@ -29,7 +37,20 @@ public class UserController {
         return ResponseEntity.ok(resp);
     }
 
+    // get current user details by token
+    @GetMapping("/cur")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<?> getUserCurrent(HttpServletRequest request) {
+        Long userId = jwtUtils.getUserIdFromJwtToken(jwtUtils.extractJwtToken(request));
+        UserDetailResponse resp = userService.getUserDetailById(userId);
+        if (resp == null) {
+            return ResponseEntity.badRequest().body(new MessageResponse("user not found with id: " + userId));
+        }
 
+        return ResponseEntity.ok(resp);
+    }
+
+    // get all users preview
     @GetMapping
     public ResponseEntity<?> getAllUsers() {
         UsersPreviewsResponse upr = new UsersPreviewsResponse();
